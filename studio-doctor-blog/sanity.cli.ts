01710f56e8466f922b9f27eeb38280a17bb96db6
Abhishek <abhishekr261002@gmail.com>
2026-04-21 12:37:50 +0530

    blogs setup

--- studio-doctor-blog/sanity.cli.ts
+++ studio-doctor-blog/sanity.cli.ts
@@ -6,6 +6,9 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    /* The ID provided by Sanity to link your local code to the deployed URL */
+    appId: 'ndvzxpqa3ye7vgqpgra0h3sm',
+    
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
